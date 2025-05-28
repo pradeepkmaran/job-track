fb4f2a255ec2fe6acc11c8175d2927250f602485
Rocketane <bis@BISs-MacBook-Pro.local>
2025-05-28 15:13:32 +0530

    Restore files from removed commit

--- backend/src/main/java/com/rocketlane/careerlog/repository/UserRepository.java
+++ backend/src/main/java/com/rocketlane/careerlog/repository/UserRepository.java
@@ -3,9 +3,4 @@ package com.rocketlane.careerlog.repository;
 import com.rocketlane.careerlog.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-    Optional<UserEntity> findByUsername(String username);
-    Optional<UserEntity> findByEmail(String email);
-}
+public interface UserRepository extends JpaRepository<UserEntity, String> { }
